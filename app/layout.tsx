207e0cb69629383eaa52af84e76b30a03428c83d
oolaoluwatobi <58662625+oolaoluwatobi@users.noreply.github.com>
2024-04-22 10:32:29 +0100

    Update title and description in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 const aBeeZee = ABeeZee({ weight: "400", subsets: ["latin"], style: "italic" });
 
 export const metadata: Metadata = {
-  title: "zwilt - Punch assessment created by ọlá",
+  title: "zwilt - oolaoluwatobi",
   description: "Punch assessment created by ọlá",
 };
 
